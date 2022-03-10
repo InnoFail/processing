20b48147177d23b4495587cf5b4f7109d3ba1cef
Inno <64632098+InnoFail@users.noreply.github.com>
2022-03-10 12:02:01 +0545

    Create index.js

---
+++ p5js/examples/index.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/addons/p5.sound.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8" />
+  <style>
+      html, body {
+      margin: 0;
+      padding: 0;
+      }
+      canvas {
+      display: block;
+      }
+
+   </style>
+  </head>
+  <body>
+    <script src="ui.js"></script>
+    <script src="group.js"></script>
+    <script src="help.js"></script>
+    <script src="sketch.js"></script>
+  </body>
+</html>
