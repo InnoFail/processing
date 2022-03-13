1e3c702a644e9f86bdc16e75d9037d9c08ee63b6
Inno <64632098+InnoFail@users.noreply.github.com>
2022-03-13 16:29:48 +0545

    used component.js

--- p5js/examples/different_components.js
+++ p5js/examples/different_components.js
@@ -1,4 +1,4 @@
-let u,v;
+let u,v,s,t,w;
 let n=10,m = 10;
 let h;
 let u_str = ";You ;can ;say ;so ;as.";
@@ -10,33 +10,38 @@ function setup(){
   h = new help();
   u = new group(new ui().set_color(h.n_red));
   u.ui.grid(50,50);
-  v = u.copy();
-  v.copy();
-  let p = v.child(0).ui;
-  p.set_str("Button").set_font_size(16);
-  v.ui.set_snap_px(0,0,p.get_text_width()+32,p.lineh+16).set_radius_px(4);
-  p.set_scroll(-16,-8).set_color(h.white).set_radius_px(4);
-  v.ui.del_snap_px(100,100,100,100).set_color(h.opacity(h.black,125));
-  p.del_snap_px(-3,-3,-3,-3);
-  //v.ui.set_snap_px(0,0,100,100).set_color(h.white);
+  v = new slider(0,100,false,u);
+  v.slider.ui.set_snap(0,46,50,50);
+  u.copy();
+  u.child(1).ui.set_snap(0,40,5,46).set_color(h.n_yellow);
+  s = new button("Button",u);
+  s.button.ui.set_snap(2,2,s.button.ui.snaps[2]+2,s.button.ui.snaps[3]+2);
+  
+  
+  u.copy().ui.set_color(h.white).set_snap(0,0,5,2).set_str("hello1");
+  u.copy().ui.set_color(h.white).set_snap(0,0,5,2).set_str("hello2");
+  u.copy().ui.set_color(h.white).set_snap(0,0,5,2).set_str("hello3");
+  t = new list(2,[u.child(3),u.child(4),u.child(5)],u);
+  t.list.ui.set_snap(10,10,10,10);
+  t.change(1,true);
 }
 
 
 function draw(){
   //textFont(font);
-  if(v.child(0).ui.clicked()){
-    let p = v.child(0).ui;
-    v.ui.set_color(h.opacity(h.black,0));
-    p.set_snap_px(0,0,p.width,p.height);
+  if(s.button.ui.clicked()){ 
+    s.shadow(0,0,0,0,h.grey);
+    t.change(1,false);
   }else{
-    let p = v.child(0).ui;
-    v.ui.set_color(h.opacity(h.black,125));
-    p.set_snap_px(-3,-3,p.width-3,p.height-3);
-    
+    s.shadow(3,3,3,3,h.grey);
+    t.change(1,true);
   }
+  u.child(1).ui.set_str(v.get_value()).set_scroll(-8,-8);
   u.ui.draw();
-  v.ui.draw();
-  v.child(0).ui.draw();
+  v.draw();
+  u.child(1).ui.draw();
+  s.draw();
+  t.draw();
 }
 
 
