6550d40c31145b11fdc98a7b6324587ad681d1bc
Inno <64632098+InnoFail@users.noreply.github.com>
2022-03-30 21:37:19 +0545

    Update poly_detection.js

--- p5js_all_libraries/matrix/poly_detection.js
+++ p5js_all_libraries/matrix/poly_detection.js
@@ -4,7 +4,6 @@
 
 let points = {
 }
-let hold = [];
 let p = [150,150];
 
 function setup() {
@@ -16,53 +15,12 @@ function setup() {
     points[count].push(height/2+random(radius)*sin(i*PI/180));
   }
   
-  for(let k=0 ; k<width; k++){
-    hold.push([]);
-    for(let j=0; j<height ; j++){
-      hold[k].push([]);
-      hold[k][j] = 0;
-    }
-  }
   
-  let s1 = 0;
-  let keys = Object.keys(points);
-  for(let i=0; i<keys.length; i++){
-    let p1 = points[keys[i]];
-    let p2 = points[keys[ i == keys.length-1 ? 0 : i+1]];
-    circle(p1[0],p1[1],20);
-    line(p1[0],p1[1],p2[0],p2[1]);
-    let u1 = createVector(p1[0],p1[1]);
-    let u2 = createVector(p2[0],p2[1]);
-    let u = createVector(p[0],p[1]);
-    let z1 = p5.Vector.sub(u1,u);
-    let z2 = p5.Vector.sub(u2,u);
-    let angle = z1.angleBetween(z2)*180/PI;
-    s1 += angle;
-    //console.log(angle);
-    for(let k=0 ; k<width; k++){
-    for(let j=0; j<height ; j++){
-      let u3 = createVector(k,j);
-      z1 = p5.Vector.sub(u1,u3);
-      z2 = p5.Vector.sub(u2,u3);
-      angle = z1.angleBetween(z2)*180/PI;
-      hold[k][j] += angle;
-    }
-  }
-  }
 }
 
 function draw() {
   background(220);
   s1 = polyDetection(points);
-  fill(0,255,0);
-  noStroke();
-  for(let k=0 ; k<width; k++){
-    for(let j=0; j<height ; j++){
-      if(hold[k][j] == 0){
-        circle(k,j,1);
-      }
-    }
-  }
   fill(0);
   text(s1,p[0]-15,p[1]+25);
   [p[0],p[1]] = [mouseX,mouseY];
