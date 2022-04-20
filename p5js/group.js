0296371a9921b662cafb64730731909c8b47a9d0
Inno <64632098+InnoFail@users.noreply.github.com>
2022-04-20 13:05:24 +0545

    Update group.js

--- p5js/group.js
+++ p5js/group.js
@@ -9,6 +9,22 @@ function group(u){
     this.childs.push(p);
     return p;
   }
+  
+  this.add_child = function(ch){
+    ch.parent = this;
+    ch.ui.p = this.ui;
+    this.childs.push(ch);
+    return this;
+  }
+  
+  
+  this.remove_child = function(index){
+    this.child(index).parent = null;
+    let p = this.child(index);
+    this.childs.splice(index,1);
+    return p;
+  }
+  
   this.child = function(index){
     return this.childs[index];
   }
